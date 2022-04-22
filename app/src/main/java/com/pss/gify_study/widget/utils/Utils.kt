a8590cdf39883684f7f1ed43606c96d7f52d8ded
choiyunsung <12345ggh81@gmail.com>
2022-04-22 16:02:03 +0900

    [DELETE] Utils 파일 변경 및 삭제

---
+++ app/src/main/java/com/pss/gify_study/widget/utils/Utils.kt
@@ -0,0 +1,7 @@
+package com.pss.gify_study.widget.utils
+
+object Utils {
+    const val BASE_URL = "https://api.giphy.com"
+    const val END_URL = "/v1/gifs/trending"
+    const val GIPHY_API_KEY = "cathZHoWoY5Ys3vWx93G56mBCfu7OkpB"
+}
