8ec9b93b0d122896be1606bfcc9cfc5e67071a61
choiyunsung <12345ggh81@gmail.com>
2022-04-25 20:43:29 +0900

    [UPDATE] api data 추가

--- app/src/main/java/com/pss/gify_study/repository/MainRepository.kt
+++ app/src/main/java/com/pss/gify_study/repository/MainRepository.kt
@@ -5,12 +5,13 @@ import com.pss.gify_study.data.remote.model.GiphyResponse
 import com.pss.gify_study.widget.utils.Utils.GIPHY_API_KEY
 import retrofit2.Response
 import retrofit2.http.Query
+import java.util.*
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
    private val giphyApi: GiphyApi
 ){
 
-    suspend fun getGiphyApi() : Response<GiphyResponse> = giphyApi.getGiphyGif(key = GIPHY_API_KEY, limit = 20, offset = 0, lang = "ko","messaging")
+    suspend fun getGiphyApi(key : String, limit : Int, offset : String, lang : String, bundle: String) : Response<GiphyResponse> = giphyApi.getGiphyGif(key = key, limit = limit, offset = offset, lang = lang,bundle)
 
 }
