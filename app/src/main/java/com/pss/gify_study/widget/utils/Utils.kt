4a5710de59702507342e3856b99288783050d6e8
choiyunsung <12345ggh81@gmail.com>
2022-04-25 20:43:48 +0900

    [UPDATE] paging starting page index 추가

--- app/src/main/java/com/pss/gify_study/widget/utils/Utils.kt
+++ app/src/main/java/com/pss/gify_study/widget/utils/Utils.kt
@@ -4,4 +4,5 @@ object Utils {
     const val BASE_URL = "https://api.giphy.com"
     const val END_URL = "/v1/gifs/trending"
     const val GIPHY_API_KEY = "cathZHoWoY5Ys3vWx93G56mBCfu7OkpB"
+    const val STARTING_PAGE_INDEX = 0
 }
